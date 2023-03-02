e1d04c5f730157378f331b653213363b497be112
hdugan <hilarydugan@gmail.com>
2023-03-02 14:19:17 -0600

    weibull function maybe working

--- src/1_raw_data_process/02_secchi.R
+++ src/1_raw_data_process/02_secchi.R
@@ -1,6 +1,6 @@
 
 secchi <- function(ice_file, path_out) {
-  # Updated 2022-12-28
+  # Updated 2023-02-02 - HD
   
   #################### LOAD DATA ####################
   inUrl1 <- "https://pasta.lternet.edu/package/data/eml/knb-lter-ntl/31/29/5a5a5606737d760b61c43bc59460ccc9"
@@ -27,7 +27,6 @@ secchi <- function(ice_file, path_out) {
     filter(metric == 'iceon') |> 
     select(lakeid, year4 = year, firstice = daynum)
   
-  
   secchi = LTERsecchi |> select(lakeid:sampledate, secnview, ice) |> 
     filter(!is.na(secnview)) |> 
     left_join(iceOff) |> 
@@ -37,32 +36,54 @@ secchi <- function(ice_file, path_out) {
     group_by(lakeid, year4) |> 
     mutate(n = n()) |> 
     filter(!n < 10) |> # filter out low year
-    ungroup() |> select(-n)
+    ungroup() |> select(-n) |> 
+    rename(year = year4)
   
+  
+  yearCombos <- crossing(lakeid = unique(secchi$lakeid), year = unique(secchi$year))
+  
+  # Openwater Secchi Maximum 
   s.openwater.max = secchi |> 
-    group_by(lakeid, year4) %>% 
+    group_by(lakeid, year) %>% 
     slice_max(secnview, with_ties = FALSE, n = 1) %>% # if ties, select the first 
     mutate(metric = "secchi_max") %>% 
-    select(lakeid, metric, sampledate, year4, daynum, secnview)
+    select(lakeid, metric, sampledate, year, daynum, secnview)
   
+  weibull.list = purrr::map2(yearCombos$lakeid, yearCombos$year, weibull, df = secchi, var = 'secnview', find = 'max')
+  weibull.df = do.call(rbind.data.frame, weibull.list)
+  
+  s.openwater.max = s.openwater.max |> left_join(weibull.df)
+  
+  # Openwater Secchi Minimum 
   s.openwater.min = secchi |> 
-    group_by(lakeid, year4) %>% 
+    group_by(lakeid, year) %>% 
     slice_min(secnview, with_ties = FALSE, n = 1) %>% # if ties, select the first 
     mutate(metric = "secchi_min") %>% 
-    select(lakeid, metric, sampledate, year4, daynum, secnview)
+    select(lakeid, metric, sampledate, year, daynum, secnview)
   
+  weibull.list = purrr::map2(yearCombos$lakeid, yearCombos$year, weibull, df = secchi, var = 'secnview', find = 'min')
+  weibull.df = do.call(rbind.data.frame, weibull.list)
+  
+  s.openwater.min = s.openwater.min |> left_join(weibull.df)
+  
+  # Spring Secchi Maximum 
   s.spring.max = secchi |> 
     filter(yday(sampledate) < 200) |>
-    group_by(lakeid, year4) %>% 
+    group_by(lakeid, year) %>% 
     slice_max(secnview, with_ties = FALSE, n = 1) %>% # if ties, select the first 
     mutate(metric = "secchi_springmax") %>% 
-    select(lakeid, metric, sampledate, year4, daynum, secnview)
+    select(lakeid, metric, sampledate, year, daynum, secnview)
   
+  weibull.list = purrr::map2(yearCombos$lakeid, yearCombos$year, weibull, df = secchi |> filter(yday(sampledate) < 200), 
+                             var = 'secnview', find = 'max', datacutoff = 6)
+  weibull.df = do.call(rbind.data.frame, weibull.list)
   
+  s.spring.max = s.spring.max |> left_join(weibull.df)
+  
+  # Join datasets
   secchi.out = s.openwater.max |> bind_rows(s.openwater.min) |> 
     bind_rows(s.spring.max) |> 
-    select(-secnview) |> 
-    rename(year = year4)
+    select(-secnview) 
   
   write_csv(secchi.out, path_out)
 
--- src/weibullFunction.R
+++ src/weibullFunction.R
@@ -1,16 +1,13 @@
-secchi
-
-lakenames = unique(secchi$lakeid)
-years = unique(secchi$year4)
-weibull.secchi.list = list()
-
-weibull <- function(lakeid, year, df, var) {
-    usedata = df |> filter(lakeid == lakeid, year4 == year) 
-
-
-    if (nrow(usedata) < 8) {
-      output = data.frame(lakeid = lakenames[i], year = years[ii], dayWeibull = NA, r2 = NA)
-      next
+weibull <- function(uselake, useyear, df, var, find = 'max', datacutoff = 8) {
+    
+  usedata = df |> filter(lakeid == uselake, year == useyear) 
+    if (nrow(usedata) < datacutoff) {
+      output = data.frame(lakeid = uselake, year = useyear, dayWeibull = NA, weibull.r2 = NA)
+      return(output)
+    }
+    
+    if (find == 'min') {
+      usedata[,var] = usedata[,var] * -1 + max(usedata[,var], na.rm = T)
     }
 
     # plot(usedata$daynum, usedata$secnview, col = 'blue', pch = 16)
@@ -22,17 +19,18 @@ weibull <- function(lakeid, year, df, var) {
     }, error=function(e) e)
 
     if(inherits(possibleError, "error")) {
-      output = data.frame(lakeid = lakenames[i], year = years[ii], dayWeibull = -999, r2 = -999)
+      output = data.frame(lakeid = uselake, year = useyear, dayWeibull = -999, weibull.r2 = -999)
     } else {
       r2 = res$r2
       dayWeibull = res$fit |> filter(f == max(f)) |> pull(x)
       # plot(res, xmin = 50, xmax = 365)
       # plot(res$fit$x, res$fit$y)
       # points(usedata$daynum, usedata$secnview, col = 'blue', pch = 16)
-      output = data.frame(lakeid = lakenames[i], year = years[ii], dayWeibull = dayWeibull, r2 = r2)
+      output = data.frame(lakeid = uselake, year = useyear, dayWeibull = dayWeibull, weibull.r2 = r2)
     }
 
     return(output)
 }
 
-weibull(df = secchi, lakeid = 'ME', year = 2012, var = 'secnview')
+weibull(df = secchi |> filter(yday(sampledate) < 200), uselake = 'ME', useyear = 2008, var = 'secnview', find = 'max')
+weibull(df = secchi, uselake = 'ME', useyear = 2012, var = 'secnview', find = 'max')
